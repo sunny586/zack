8ca51be2063dd15384ffb1891b60fa10fa22204d
zhangyu <313283928@qq.com>
2024-09-05 21:09:33 +0800

    perf: 优化build.cjs

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -4,8 +4,9 @@ export const head: HeadConfig[] = [
   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-  ['link', { rel: 'apple-touch-icon', href: '/favicon.ico' }],
-  ['link', { rel: 'mask-icon', href: '/favicon.ico', color: '#3eaf7c' }],
-  ['meta', { name: 'msapplication-TileImage', content: '/favicon.ico' }],
+  ['link', { rel: 'icon', href: '/zack/favicon.ico' }],
+  ['link', { rel: 'apple-touch-icon', href: '/zack/favicon.ico' }],
+  ['link', { rel: 'mask-icon', href: '/zack/favicon.ico', color: '#3eaf7c' }],
+  ['meta', { name: 'msapplication-TileImage', content: '/zack/favicon.ico' }],
   ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
 ]

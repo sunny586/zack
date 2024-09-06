470318701c6052dda5a97e43bf715d5c8fbef781
zhangyu <313283928@qq.com>
2024-09-06 20:00:26 +0800

    perf: 优化deploy

--- docs/.vitepress/configs/const/be-category.ts
+++ docs/.vitepress/configs/const/be-category.ts
@@ -2,7 +2,6 @@ export const BE_CATEGORY = [
   { text: '后端知识树', link: '/be/' },
   {
     text: 'java',
-    // collapsed: true,
     items: [{ text: 'jdk21新特性', link: '/be/java/jdk21新特性' }],
   },
 ]

6dbf16fd8f8f5105f3f74f7e86ba61054ca3e571
zhangyu <313283928@qq.com>
2024-09-10 15:43:53 +0800

    解决冲突

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,85 @@
+import { basename } from 'node:path'
+import { defineConfig } from 'vitepress'
+import MarkdownPreview from 'vite-plugin-markdown-preview'
+
+import { head, nav, sidebar } from './configs'
+
+const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
+
+console.log(process.env.GITHUB_REPOSITORY, 'process.env.GITHUB_REPOSITORY...')
+
+export default defineConfig({
+  outDir: '../dist',
+  base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
+
+  lang: 'zh-CN',
+  title: '茂茂物语',
+  description: '茂茂的成长之路，包含前端常用知识、源码阅读笔记、各种奇淫技巧、日常提效工具等',
+  head,
+
+  lastUpdated: true,
+  cleanUrls: true,
+
+  /* markdown 配置 */
+  markdown: {
+    lineNumbers: true,
+  },
+
+  /* 主题配置 */
+  themeConfig: {
+    i18nRouting: false,
+
+    logo: '/logo.png',
+
+    nav,
+    sidebar,
+
+    /* 右侧大纲配置 */
+    outline: {
+      level: 'deep',
+      label: '目录',
+    },
+
+    socialLinks: [{ icon: 'github', link: 'https://github.com/sunny586/vitepress-nav-template' }],
+
+    footer: {
+      message: '如有转载或 CV 的请标注本站原文地址',
+      copyright: 'Copyright © 2019-present maomao',
+    },
+
+    lastUpdated: {
+      text: '最后更新于',
+      formatOptions: {
+        dateStyle: 'short',
+        timeStyle: 'medium',
+      },
+    },
+
+    docFooter: {
+      prev: '上一篇',
+      next: '下一篇',
+    },
+
+    returnToTopLabel: '回到顶部',
+    sidebarMenuLabel: '菜单',
+    darkModeSwitchLabel: '主题',
+    lightModeSwitchTitle: '切换到浅色模式',
+    darkModeSwitchTitle: '切换到深色模式',
+
+    /*** 自定义配置 ***/
+    visitor: {
+      badgeId: 'sunny586.vitepress-nav-template',
+    },
+
+    comment: {
+      repo: 'sunny586/zack',
+      repoId: 'R_kgDOMkRzkg',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDOMkRzks4ChvEk',
+    },
+  },
+
+  vite: {
+    plugins: [MarkdownPreview()],
+  },
+})

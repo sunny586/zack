9878e3b116217b3f38391f7923b9a042a4aade2f
zhangyu <313283928@qq.com>
2024-10-10 10:10:14 +0800

    ds

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ cd $dist_path
 git init
 git add -A
 git commit -m "deploy, $commit_info"
-git push -f $push_addr HEAD:$push_branch
+sudo git push -f $push_addr HEAD:$push_branch
 
 cd -
 rm -rf $dist_path
